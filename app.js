b23f03031ca42c83b4846bf7c4a3db4ff9be726e
kardo-Mohammed <kardomohammed25@gmail.com>
2025-02-16 09:57:35 +0100

    lernen meiar

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 
-var grades = [14.5,18,17,16.75,15,12,19,18.25,10,20];
+/*var grades = [14.5,18,17,16.75,15,12,19,18.25,10,20];
 let sum = 0 ;
 for (var i=0 ;i<grades.length; i++){
     sum += grades[i] ;
 }
 var average = sum/grades.length;
 console.log(average)
+**************************************
+
+var numbers = [10,-6,-2,5,12,-17,20,-12,65,-89]
+var positiveNumbers = []
+var negativeNumbers = []
+
+for(var i = 0;i < numbers.length; i++){
+    if(numbers[i] > 0){
+            positiveNumbers.push(numbers[i]);
+
+    }
+    if(numbers[i] < 0){
+        negativeNumbers.push(numbers[i]);
+    }
+    console.log(positiveNumbers);
+    console.log(negativeNumbers);
+    *******************************************
+let str = 'kardo'
+for(let k =0;k<str.length;k++){
+    console.log(str[k])
+    *********************************************
+}*/
